913320ad6b21ecb729eb7bdc3420091f967ec258
Michael Babušík <94798334+Micjei@users.noreply.github.com>
2023-11-15 20:07:44 +0100

    LOL

--- src/expression.c
+++ src/expression.c
@@ -31,62 +31,92 @@ int p_table[15][15] = {
 5 - E -> (E)
 6 - E -> i
 7 - E -> E ?? E
+8 - E -> E == E
+9- E -> E <= E
+10- E -> E >= E
+11- E -> E != E 
+12- E -> E < E
+13- E -> E > E 
 ...
 var i : (E == E)
 i = 1
 i - E -> E op E
 if((i == 1) == (a == 2))
 */
-char* najdi_pravidlo(Stack* expr_stack) {
-    char b = peek(expr_stack);
-    switch (b) {
-        case '+':
-            return "E+E";
-        case '-':
-            return "E-E";
-        case '*':
-            return "E*E";
-        case '/':
-            return "E/E";
-        case '^':
-            return "E^E";
-        case '(':
-            return "(E)";
-        default:
-            return "i";
-    }
-}
 
 void LESS(Stack* expr_stack, char a) {
-    // Najdi na zásobníku nejvrchnější terminální symbol-b
-    char b;
-    while (!isEmpty(expr_stack)) {
-        b = pop(expr_stack);
-        if (b == '+' || b == '-' || b == '*' || b == '/' || b == '^' || b == '(') {
-            break;
-        }
-    }
-
-    // Hned za symbol-b vlož do zásobníku symbol <
     push(expr_stack, '<');
-
-    // Předti symbol 'a' ze vstupu a dej jej na vrchol zásobníku
     push(expr_stack, a);
 }
 
-MORE(a, b){
+void MORE(Stack* expr_stack, char a, bool express, char symbol) {
+    if (express) {
+        switch (symbol) {
+            case '+':
+                pop(expr_stack);
+                pop(expr_stack);
+                pop(expr_stack);
+                pop(expr_stack);
+                printf("Redukce podle pravidla 1\n");
+                push(expr_stack, 'E');
+                break;
 
+            case '-':
+                pop(expr_stack);
+                printf("Redukce podle pravidla 2\n");
+                push(expr_stack, 'E');
+                break;
+
+            case '*':
+                printf("Redukce podle pravidla 3\n");
+                push(expr_stack, 'E');
+            case '/':
+                printf("Redukce podle pravidla 4\n");
+                push(expr_stack, 'E');
+            case '()':
+                printf("Redukce podle pravidla 5\n");
+                push(expr_stack, 'E');
+            case 'i':
+                printf("Redukce podle pravidla 6\n");
+                push(expr_stack, 'E');
+            case '??':
+                printf("Redukce podle pravidla 7\n");   
+                push(expr_stack, 'E');
+            case '==':
+                printf("Redukce podle pravidla 8\n");
+                push(expr_stack, 'E');
+            case '<=':
+                printf("Redukce podle pravidla 9\n");
+                push(expr_stack, 'E');
+            case '>=':
+                printf("Redukce podle pravidla 10\n");
+                push(expr_stack, 'E');
+            case '!=':
+                printf("Redukce podle pravidla 11\n");
+                push(expr_stack, 'E');
+            case '<':
+                printf("Redukce podle pravidla 12\n");
+                push(expr_stack, 'E');
+            case '>':
+                printf("Redukce podle pravidla 13\n");
+                push(expr_stack, 'E');
+            default:
+                printf("nebylo nalezeno pravidlo")
+        }
+    }
 }
 
-EQUA(a, b){
-
+void EQUA(Stack* expr_stack, char a) {
+    push(expr_stack, a);
 }
-ERRR(a, b){
-    
+
+void ERRR(a, b){
+ //syntax err   
 }
 
 
 int main() {
+    bool express = false
     Stack expr_stack;
     initializeStack(&expr_stack);
     push(&expr_stack, '$'); // Change: push character instead of string
@@ -104,6 +134,12 @@ int main() {
     while (!isEmpty(&vstupni_stack)) {
         char a = peek(&vstupni_stack);
         char b = peek(&expr_stack);
+        if (b == 'E'){
+            pop(&expr_stack);
+            b = peek(&expr_stack);
+            push(&expr_stack, 'E');
+            express = true;
+        }
         
         int surx = -1; int sury = -1;
         
@@ -120,9 +156,14 @@ int main() {
         }
 
         if((sury == -1) || (surx == -1)){
-            fprintf(stderr,"Znak z expression nie je validny") //error vypis ak nenajdem znak v tabulke
+            fprintf(stderr,"Znak z expression nie je validny"); //error vypis ak nenajdem znak v tabulke
         }
-
+        switch(p_table[surx][sury]){
+            case 'MORE': MORE(expr_stack, a, express, b); break;
+            case 'LESS': LESS(expr_stack, a); break;
+            case 'EQUA': EQUA(expr_stack, a); break;
+            case 'ERRR': fprintf(stderr,"Nespravna kombinacia tokenov ktora vedie k errorovemu stavu"); break; //error vypis ak nenajdem znak v tabulke
+        }
         
         
     }
