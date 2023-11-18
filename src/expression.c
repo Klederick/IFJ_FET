d3da9004fade85fb7220e3bc8366a906fc7f0019
Michael Babušík <94798334+Micjei@users.noreply.github.com>
2023-11-18 02:45:38 +0100

    NEFUNGUJE TO

--- src/expression.c
+++ src/expression.c
@@ -1,7 +1,9 @@
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include "stringstack.c"
+#include <stdbool.h>
 
 #define TABLE_SIZE 16
 
@@ -19,7 +21,8 @@
 10 - E -> E >= E
 11 - E -> E != E 
 12 - E -> E < E
-13 - E -> E > E */
+13 - E -> E > E 
+*/
 
 char* p_table[16][16] = {
     {"x", "+", "-", "*", "/", "(", "i", ")", "$", "==", "<=", ">=", ">", "!=", "<", "??"},
@@ -46,14 +49,14 @@ int findStringInColumn(const char* a) {
     // Procházení prvního řádku tabulky
     for (i = 0; i < 16; i++) {
         if (strcmp(a, p_table[0][i]) == 0) { // Porovnání řetězce 'a' s hodnotou v tabulce
-            printf("Found '%s' at position [%d][%d] in p_table\n", a, 0, i);
+            printf("příchozí je '%s' at position [%d][%d] in p_table\n", a, 0, i);
             found = 1;
             break;
         }
     }
 
     if (!found) {
-        printf("String '%s' not found in p_table\n", a);
+        printf("příchozí String '%s' not found in p_table\n", a);
         // return string ....
     }
     return i;
@@ -65,7 +68,7 @@ int findStringInRow(const char* b) {
     // Procházení řádku tabulky
     for (i = 0; i < 16; i++) {
         if (strcmp(b, p_table[i][0]) == 0) { // Porovnání řetězce 'a' s hodnotou v tabulce
-            printf("Found '%s' at position [%d][%d] in p_table\n", b, i, 0);
+            printf("na expr je '%s' at position [%d][%d] in p_table\n", b, i, 0);
             found = 1;
             break;
         }
@@ -86,32 +89,58 @@ char* getStringFromCoordinates(int col, int row) {
     }
 }
 
-void add(StringStack* expr_stack, const char* a){
-    pushString(expr_stack, "<");
-    pushString(expr_stack, a);
+void add(StringStack* expr_stack, const char* a, bool E){
+    if(E){
+        printf("mame ecko\n");
+        popString(expr_stack);
+        pushString(expr_stack, "<");
+        pushString(expr_stack, "E");
+        pushString(expr_stack, a);
+    }
+    else{
+        pushString(expr_stack, "<");
+        pushString(expr_stack, a);
+    }
 }
-void reduce(StringStack* expr_stack, const char* a){
+void reduce(StringStack* expr_stack, const char* a, bool E, StringStack* vstupni_stack){
     char b[MAX_STRING_LENGTH];
     char c[MAX_STRING_LENGTH];
     if (strcmp(a,"+") == 0 || strcmp(a,"-") == 0 || strcmp(a,"*") == 0 || strcmp(a,"/") == 0){
-        popString(expr_stack);
-        popString(expr_stack);
-        popString(expr_stack);
+        if(E){
+            popString(expr_stack);
+            popString(expr_stack);
+            popString(expr_stack);
+            popString(expr_stack);
+        }
+        else{
+            popString(expr_stack);
+            popString(expr_stack);
+        }
         printf("Redukce podle pravidla 1-4\n");
         pushString(expr_stack, "E");   
     }
     else if (strcmp(a,")") == 0){
         // Check for "("
-        strcpy(b, peekString(expr_stack));
-        popString(expr_stack);
-        strcpy(c, peekString(expr_stack));
-        popString(expr_stack);
-        if (strcmp(c,"(") != 0) {
-            printf("Chybi zavorka\n");
+        if(E){
+            printf("MAME TU ECKO\n");
+
+            strcpy(c, peekString(expr_stack));
+            popString(expr_stack);
+
+            if (strcmp(c,"(") != 0) {
+                printf("Chybi zavorka\n");
+            }
+            popString(expr_stack);
+            popString(expr_stack);
+            printf("Redukce podle pravidla 5\n");
+            pushString(expr_stack, "E");
+            popString(vstupni_stack);
         }
-        popString(expr_stack);
-        printf("Redukce podle pravidla 5\n");
-        pushString(expr_stack, "E");
+        else{
+            popString(expr_stack);
+            popString(expr_stack);
+            pushString(expr_stack, "E");
+        }
     }
     else if (strcmp(a, "??") == 0 || strcmp(a, "==") == 0 || strcmp(a, "<=") == 0 || strcmp(a,">=") == 0 || strcmp(a, "!=") == 0 || strcmp(a, ">") == 0 || (strcmp(a,"<") == 0)){
         popString(expr_stack);
@@ -120,6 +149,22 @@ void reduce(StringStack* expr_stack, const char* a){
         popString(expr_stack);
         printf("Redukce podle pravidla nekolik 6-12\n");
         pushString(expr_stack, "E");
+    }
+    else if (strcmp(a, "$") == 0){
+        if(E){
+            popString(expr_stack);
+            popString(expr_stack);
+            popString(expr_stack);
+            popString(expr_stack);
+
+            pushString(expr_stack, "E");
+        }
+        else{
+            popString(expr_stack);
+            popString(expr_stack);
+            pushString(expr_stack, "E");
+            printf("posledni");
+        }
     }
     else{
         popString(expr_stack);
@@ -132,6 +177,7 @@ void equal(StringStack* expr_stack, const char* a){
     pushString(expr_stack, a);
 }
 int main() {
+    bool E = false;
     StringStack expr_stack;
     initializeStringStack(&expr_stack);
     pushString(&expr_stack, "$"); // Change: push character instead of string
@@ -139,15 +185,18 @@ int main() {
     StringStack vstupni_stack;
     initializeStringStack(&vstupni_stack);
 
-    // Vložení expression "i+(i*i)" na zásobník
+    // Vložení expression "(i*i)+i" na zásobník
+    pushString(&vstupni_stack, "$");
+    pushString(&vstupni_stack, "i");
+    pushString(&vstupni_stack, "+");
     pushString(&vstupni_stack, ")");
     pushString(&vstupni_stack, "i");
     pushString(&vstupni_stack, "*");
     pushString(&vstupni_stack, "i");
     pushString(&vstupni_stack, "(");
-    pushString(&vstupni_stack, "+");
-    pushString(&vstupni_stack, "i");
 
+    StringStack temp_stack;
+    initializeStringStack(&temp_stack);
     /*
     printf("Top element: %s\n", peekString(&vstupni_stack));
     printf("Popped element: %s\n", popString(&vstupni_stack));
@@ -160,11 +209,46 @@ int main() {
 
     if(strcmp(b, "$") == 0){
         while(!isStringStackEmpty(&vstupni_stack)){
+            E = false;
+            char c[MAX_STRING_LENGTH];
             char a[MAX_STRING_LENGTH];
             strcpy(a, peekString(&vstupni_stack));
             char b[MAX_STRING_LENGTH];
             strcpy(b, peekString(&expr_stack));
+            if(strcmp(a, "$") == 0){
+                popString(&expr_stack);
+                strcpy(c, peekString(&expr_stack));
+                if(strcmp(c, "$") == 0){
+                    //printf("konec expression");
+                    break;
+                }
+                pushString(&expr_stack, c);
+            }
+            if(strcmp(b, "E") == 0){
+                E = true;
+                strcpy(c, peekString(&expr_stack));
+                popString(&expr_stack);
+                strcpy(b, peekString(&expr_stack));
+                pushString(&expr_stack, c);
+                if (strcmp(b,"+") == 0 || strcmp(b,"-") == 0 || strcmp(b,"*") == 0 || strcmp(b,"/") == 0){
+                    reduce(&expr_stack, b, E, &vstupni_stack);
+                    printf("BLLBBL\n");            
+                    printf("Obsah zásobníku expr_stack:\n");
+                    while (!isStringStackEmpty(&expr_stack)) {
+                        char* item = popString(&expr_stack);
+                        pushString(&temp_stack, item);
+                    }
 
+                    // Vrácení položek zpět na zásobník vstupni_stack
+                    while (!isStringStackEmpty(&temp_stack)) {
+                        char* item = popString(&temp_stack);
+                        printf("%s ", item);
+                        pushString(&expr_stack, item);
+                    }
+                    printf("\n");
+                    printf("BLLBBL\n\n");            
+                }
+            }
             int positionx = findStringInColumn(a);
             int positiony = findStringInRow(b);
             char* symbol = getStringFromCoordinates(positionx, positiony);
@@ -173,10 +257,11 @@ int main() {
 
             if(strcmp(symbol, "L") == 0){
                 popString(&vstupni_stack);
-                add(&expr_stack, a);
+                printf("popnul jsem vstupni stack\n");
+                add(&expr_stack, a, E);
             }
             else if(strcmp(symbol, "M") == 0){
-                reduce(&expr_stack, a);
+                reduce(&expr_stack, a, E, &vstupni_stack);
             }
             else if(strcmp(symbol, "Q") == 0){
                 equal(&expr_stack, a);
@@ -187,7 +272,22 @@ int main() {
                 break;
             }
             strcpy(b, peekString(&expr_stack));
-            printf("%s\n", b);
+            printf("Obsah zásobníku expr_stack:\n");
+            while (!isStringStackEmpty(&expr_stack)) {
+                char* item = popString(&expr_stack);
+                pushString(&temp_stack, item);
+            }
+
+            // Vrácení položek zpět na zásobník vstupni_stack
+            while (!isStringStackEmpty(&temp_stack)) {
+                char* item = popString(&temp_stack);
+                printf("%s ", item);
+                pushString(&expr_stack, item);
+            }
+            
+            strcpy(a, peekString(&vstupni_stack));
+            printf("\nObsah zásobníku vstupni_stackX:  %s\n", a);
+            printf("\n\n");
         }
     }
     return 0;
