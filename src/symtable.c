b880f6ae0a5e15de687659cf7215e23858eecdeb
Michael Babušík <94798334+Micjei@users.noreply.github.com>
2023-12-06 22:56:29 +0100

    ex

--- src/symtable.c
+++ src/symtable.c
@@ -1,6 +1,5 @@
 #ifndef SYMTABLE_C
 #define SYMTABLE_C
-//  include
 #include <string.h>
 
 int max(int a, int b) {
@@ -70,7 +69,7 @@ tNode_t insert(tNode_t node, char *key, int nodeType) {
     } else if (strcmp(key, node->key) > 0) {
         node->rptr = insert(node->rptr, key, nodeType);
     } else {
-        return node; // Duplicitní klíč, nic se nemění
+        return node;
     }
 
     node->height = 1 + max(height(node->lptr), height(node->rptr));
@@ -143,47 +142,13 @@ void destroyTree(tNode_t tree) {
         return;
     }
 
-    // Nejprve rekurzivně uvolníme podstromy (levý a pravý)
     destroyTree(tree->lptr);
     destroyTree(tree->rptr);
 
-    // Nakonec uvolníme paměť pro uzel
     free(tree);
 }
 
 tNode_t initializeTree() {
-    return NULL; // Vracíme prázdný strom
+    return NULL;
 }
-/*
-int main() {
-    tNode_t root = initializeTree();
-
-    symtabInsert(&root, "func1", 1);
-    symtabInsert(&root, "func2", 1);
-    symtabInsert(&root, "varB", 2);
-    symtabInsert(&root, "varA", 2);
-    symtabInsert(&root, "varC", 2);
-    symtabInsert(&root, "varD", 2);
-    symtabInsert(&root, "varE", 2);
-    symtabInsert(&root, "varF", 2);
-    symtabInsert(&root, "varG", 2);
-    symtabInsert(&root, "varH", 2);
-
-    printf("Inorder Traversal:\n");
-    symtabInorder(root);
-
-    // Vyhledávání uzlu s klíčem "varA"
-    char *search_key = "var";
-    tNode_t result = search(root, search_key);
-    if (result != NULL) {
-        printf("\nSearch Result: Key: %s, NodeType: %d\n", result->key, result->nodeType);
-    } else {
-        printf("\nNode with key %s not found.\n", search_key);
-    }
-
-    // Zde byste mohli provádět další operace s AVL stromem
-
-    destroyTree(root);
-    return 0;
-}*/
 #endif
